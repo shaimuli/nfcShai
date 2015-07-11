f8a8e8324790fcd7e9cf7bebc85d9cc74b4da3f0
shaimuli <shaimuli@gmail.com>
2015-07-12 00:10:15 +0300

    asdas
    asd

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,60 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
+        "id": "org.apache.cordova.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
+        "id": "org.apache.cordova.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.device-orientation": "0.3.11",
+    "com.chariotsolutions.nfc.plugin": "0.6.2"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/scripts/index.js
@@ -0,0 +1,93 @@
+﻿//// For an introduction to the Blank template, see the following documentation:
+//// http://go.microsoft.com/fwlink/?LinkID=397704
+//// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+//// and then run "window.location.reload()" in the JavaScript Console.
+//(function () {
+//    "use strict";
+    
+
+//    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+//    nfc.addNdefListener(
+//      function (nfcEvent) {
+//          console.log('nfcEvent ' + JSON.stringify(nfcEvent));
+//          var tag = nfcEvent.tag,
+//            ndefMessage = tag.ndefMessage;
+
+//          // dump the raw json of the message
+//          // note: real code will need to decode
+//          // the payload from each record
+//          console.log(JSON.stringify(ndefMessage));
+
+//          // assuming the first record in the message has 
+//          // a payload that can be converted to a string.
+//          console.log(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+//          alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+//      },
+//      function () { // success callback
+         
+//          console.log("Waiting for NDEF tag");
+//      },
+//      function (error) { // error callback
+//          console.log("Error adding NDEF listener " + JSON.stringify(error));
+//      }
+//    );
+
+//    function onDeviceReady() {
+//        // Handle the Cordova pause and resume events
+//        document.addEventListener( 'pause', onPause.bind( this ), false );
+//        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+//        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+//    };
+
+//    function onPause() {
+//        // TODO: This application has been suspended. Save application state here.
+//    };
+
+//    function onResume() {
+//        // TODO: This application has been reactivated. Restore application state here.
+//    };
+//} )();
+//
+
+var app = {
+    initialize: function () {
+        this.bindEvents();
+    },
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function () {
+        var success = function () {
+            console.log("addNdefListener success");
+        };
+        var failure = function (reason) {
+            alert("addNdefListener failed " + reason);
+        }
+
+        // The NDEF listener runs in the foreground
+        nfc.addNdefListener(app.onNfc, success, failure);
+
+        // The Mime-Type listener is required to handle NDEF messages that
+        // launch the app from an intent filters in AndroidManifest.xml.
+        // For messages from intents, the mime type defined here doesn't matter.
+        // Note the same event handler is used for NDEF and Mime
+        nfc.addMimeTypeListener('text/any', app.onNfc, success, failure);
+    },
+    onNfc: function (nfcEvent) {
+        var tag = nfcEvent.tag,
+            ndefMessage = tag.ndefMessage;
+
+        // dump the raw json of the message
+        // note: real code will need to decode
+        // the payload from each record
+        alert(JSON.stringify(ndefMessage));
+
+        // show the payload of the first record as a string
+        // might produce junk depending on the record type
+        alert(nfc.bytesToString(ndefMessage[0].payload).substring(1));
+
+    }
+};
+
+app.initialize();
